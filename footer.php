2d8781a0dc49a420ee8f6fae113ce3903dfe0a33
AMARJEET GUPTA <123367121+AmarjeetGupta123@users.noreply.github.com>
2024-12-06 05:15:48 +0530

    Add files via upload
    this is a jayanti solar project

---
+++ footer.php
@@ -0,0 +1,128 @@
+   <!-- Footer Start -->
+
+   <div class="container-fluid  text-body footer  pt-1 wow fadeIn" style="background: linear-gradient(168deg,rgb(248, 228, 228),rgb(127, 127, 165),rgb(116, 101, 101));" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-4 col-md-6">
+                    <div class="h-75 w-75">
+                        <img width="100%" src="img/jyanti/logo-jyanti-removebg-preview.png" alt="">
+                    </div>
+                    <!-- <h5 class="text-white mb-2">Follow Us</h5> -->
+                    <div class="d-flex mt-2 pt-2">
+                        <a style="background-color: black;" class="btn btn-square  btn-outline-light btn-social" href=""><i class="bi bi-twitter-x"></i></a>
+                        <a style="background-color: blue;" class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-square btn-outline-light bg-white btn-social" href=""><i style="color: red;" class="fab fa-youtube"></i></a>
+                        <a style="background-color: blue;" class="btn  btn-square btn-outline-light btn-social" href=""><i  class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text-white mb-4">Company</h5>
+                    <a class="btn" href="about.html">About Us</a>
+                    <a class="btn " href="subsidy.html">Subsidy</a>
+                    <a class="btn " href="product.html">Product</a>
+                    <a class="btn " href="channel.html"> Channel</a>
+                    <a class="btn " href="contact.html">Contact Us</a>
+                </div>
+
+             
+
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-4">Other</h5>
+                    <a class="btn " href="on.html">On Grid Solar Power Plant</a>
+                    <a class="btn " href="off.html">Off Grid Solar Power Plant</a>
+                    <a class="btn " href="hybrid.html">Hybrid Solar Power Plants</a>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-4">Address</h5>
+                    <p class="mb-2 text-white "><i class="fa fa-map-marker-alt me-3"></i>B-25-G-4/2369, St. No. 1, New Vishnupuri, New Shivpuri Road, Ludhiana-141007</p>
+                    <p class="mb-2 text-white"><i class="fa fa-phone-alt me-3"></i>+91 9216086981</p>
+                    <p class="mb-2 text-white"><i class="fa fa-phone-alt me-3"></i>+91 9914586981</p>
+
+                    <p class="text-white" ><i class="fa fa-envelope "></i> jyantienterprises@gmail.com</p>
+                   
+                </div>
+
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-12 text-center text-md-center mb-3 mb-md-0" style="color: rgb(238, 223, 223);" >
+                        &copy; Copyright <a href="#">Jyanti Solar </a>, All Right Reserved.  Designed &
+                      
+                        Distributed By <a href="https://www.bhoomitechzone.in/" target="_blank">BTPL</a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer End -->
+
+    <!---Whatapp integration-->
+
+<div class="whatsapp-icon " onclick="openWhatsAppChat()">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp">
+</div>
+
+  <!---Whatapp integration closed-->
+
+  <!-- call icon  start -->
+
+  <div class="whatsapp-icon" style="z-index: 1000; margin-bottom: 100px;">
+      <a href="tel:+91 92160 86981">
+        <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
+
+    <dotlottie-player src="https://lottie.host/702ed976-0c02-4182-b215-7f4bc64486f5/FoYiHtZPtH.json" background="transparent" speed="1" style="width: 70px; height: 70px;" loop autoplay></dotlottie-player>
+      </a>
+</div>
+
+<!-- call icon end  -->
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square  rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- amar js  -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+    crossorigin="anonymous"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+
+    <script>
+
+        function openWhatsAppChat() {
+            // Replace with your phone number in international format
+            var phoneNumber = "919914586981";  // Example: 91 for India, followed by the number
+            var message = "Hello, I'm interested in your services!";
+            var url = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
+            window.open(url, "_blank");
+        }
+        
+            </script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title> Jyanti Solar</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/jyanti/logo-jyanti-removebg-preview.png" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+
+  
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
+        <a href="index.html" class="navbar-brand d-flex align-items-center  px-4 px-lg-5">
+            <div class=" logo-img ">
+                <img width="100%" src="img/jyanti/logo-jyanti-removebg-preview.png" alt="">
+            </div>
+            <!-- <h2 class="m-0 text-primary"> Jyanti Solar</h2> -->
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.html" class="nav-item nav-link ">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <a href="subsidy.html" class="nav-item nav-link">Subsidy</a>
+                <a href="channel.html" class="nav-item nav-link"> Channel Partner</a>
+                <div class="nav-item dropdown">
+                    <a href="product.html" class="nav-link dropdown-toggle" >Products</a>
+                    <div class="dropdown-menu bg-light m-0">
+                        <a href="on.html" class="dropdown-item">On Grid Solar Power Plant</a>
+                        <a href="off.html" class="dropdown-item">Off Grid Solar Power Plant</a>
+                        <a href="hybrid.html" class="dropdown-item">Hybrid  Solar Power Plant</a>
+                       
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Contact Us</a>
+            </div>
+
+            <a href="form.html" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Get A Quote<i class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
